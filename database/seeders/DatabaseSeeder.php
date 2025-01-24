f873ddefe9ecb9593479fdf8090811def40ff4fb
cinthiaTG <2230282@upv.edu-mx>
2025-01-24 00:16:40 -0600

    seeders de usuarios pruebas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        \App\Models\User::truncate();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('adminPass'),
+            'remember_token' => Str::random(10),
+        ]);
+        User::factory()->create([
+            'name' => 'Vanessa Torres',
+            'email' => 'vane@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('vane300'),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
